a51465ee84d069d7a3d2c84bd6847b8a8f278f91
shruti10951 <shrutipatil10951@gmail.com>
2025-09-16 22:57:35 +0530

    added a temp logo also added suppress warning for thread factory

--- android-app/app/src/main/java/com/shrujan/loomina/viewmodel/ThreadViewModelFactory.kt
+++ android-app/app/src/main/java/com/shrujan/loomina/viewmodel/ThreadViewModelFactory.kt
@@ -7,6 +7,7 @@ import com.shrujan.loomina.data.repository.ThreadRepository
 class ThreadViewModelFactory(
     private val repository: ThreadRepository
 ) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST") // Suppresses unchecked cast warning since we validate the class type.
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(CreateThreadViewModel::class.java) ->
