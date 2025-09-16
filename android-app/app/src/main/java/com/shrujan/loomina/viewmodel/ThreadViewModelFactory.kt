8b661f707308bdaf9a30fff225299f3118a2a650
shruti10951 <shrutipatil10951@gmail.com>
2025-09-16 22:21:50 +0530

    fetch all current users threads

--- android-app/app/src/main/java/com/shrujan/loomina/viewmodel/ThreadViewModelFactory.kt
+++ android-app/app/src/main/java/com/shrujan/loomina/viewmodel/ThreadViewModelFactory.kt
@@ -4,12 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.shrujan.loomina.data.repository.ThreadRepository
 
-class ThreadViewModelFactory(
+class CreateThreadViewModelFactory(
     private val repository: ThreadRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(ThreadViewModel::class.java)) {
-            return ThreadViewModel(repository) as T
+        if (modelClass.isAssignableFrom(CreateThreadViewModel::class.java)) {
+            return CreateThreadViewModel(repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
